aa29940033847594872b5e4036e6ab685157afc7
MaksimHank <orthopedicsalon5@gmail.com>
2025-10-19 23:33:31 +0300

    added service.Run() и app.Run(os.Args) в urfave/cli.

--- main.go
+++ main.go
@@ -2,18 +2,44 @@ package main
 
 import (
 	"fmt"
-	"os"
-
+	"github.com/MaksimHank/WorkingWithFile/changeTheStringContinue/logger"
 	pres "github.com/MaksimHank/WorkingWithFile/changeTheStringContinue/presenter"
 	prod "github.com/MaksimHank/WorkingWithFile/changeTheStringContinue/producer"
 	sr "github.com/MaksimHank/WorkingWithFile/changeTheStringContinue/service"
+	"github.com/urfave/cli"
+	"os"
 )
 
 func main() {
 	var inputFile string
 	var outputFile string
 
-	if len(os.Args) < 2 {
+	producer := prod.NewFileProducer(inputFile)
+	presenter := pres.NewFilePresenter(outputFile)
+
+	service := sr.NewService(producer, presenter)
+
+	app := &cli.App{
+		Name:  "",
+		Usage: "",
+		Commands: []cli.Command{
+			{
+				Name:      "Run",
+				Usage:     "My app",
+				ArgsUsage: "<input path file, out put path file is optional>",
+				Action: func(c *cli.Context) error {
+					logger.Info("Running app... (service.Run)")
+					if c.NArg() < 1 {
+						return fmt.Errorf("must be input file in argument")
+					}
+					inputFile = c.Args().Get(0)
+					return service.Run()
+				},
+			},
+		},
+	}
+
+	/*if len(os.Args) < 2 {
 		fmt.Println("go run main.go inputfile.txt(required) outputfile.txt(optional)")
 		return
 	}
@@ -23,14 +49,10 @@ func main() {
 		outputFile = os.Args[2]
 	} else {
 		outputFile = "output_default.txt"
-	}
+	}*/
 
-	producer := prod.NewFileProducer(inputFile)
-	presenter := pres.NewFilePresenter(outputFile)
-
-	service := sr.NewService(producer, presenter)
-
-	if err := service.Run(); err != nil {
-		fmt.Println("Error", err)
+	if err := app.Run(os.Args); err != nil {
+		logger.Error("Application error", err)
+		os.Exit(1)
 	}
 }
